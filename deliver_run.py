fc0bcb96efca2844e53c1b77a87266161e46a39d
Matthew Brett <matthew.brett@gmail.com>
2021-05-17 15:28:59 +0100

    First working pass

---
+++ deliver_run.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+""" Deliver stimulus run
+"""
+
+import os.path as op
+import time
+
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+
+import numpy as np
+import pandas as pd
+
+from playsound import playsound
+
+import keyboard
+
+
+def deliver_run(run_fname):
+    run_df = pd.read_csv(run_fname)
+    for i, row in run_df.iterrows():
+        for v in np.array(row):
+            if isinstance(v, str):
+                if op.isfile(v):
+                    playsound(v)
+            elif isinstance(v, (int, float)):
+                time.sleep(v / 1000)
+        if i == 24:
+            print('Press space to continue')
+            keyboard.wait(" ")
+
+
+def get_parser():
+    parser = ArgumentParser(description=__doc__,  # Usage from docstring
+                            formatter_class=RawDescriptionHelpFormatter)
+    parser.add_argument('run_fname',
+                        help='CSV file defining run')
+    return parser
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    deliver_run(args.run_fname)
+
+
+if __name__ == '__main__':
+    main()
--- make_runs.py
+++ make_runs.py
@@ -2,6 +2,7 @@
 * Two tones – low frequency (500 Hz) and high frequency (2000 Hz)
 * Tone duration 250 msec
 * Random gap between tones of 2-3 seconds.
+* Gap after trial 5s
 * Number of tones: 25 of each one
 * Number of trials: 50
 * Random presentation
@@ -19,17 +20,18 @@ import pandas as pd
 def make_run():
     # Valid trials
     types = np.repeat(['valid', 'invalid'], [40, 10])
-    tones1 = np.repeat(['tone_500', 'tone_2000'], [20, 20])
+    tones1 = np.repeat(['tone_500.wav', 'tone_2000.wav'], [20, 20])
     tones2 = tones1.copy()
-    tones1 = np.append(tones1, ['tone_500'] * 5)
-    tones2 = np.append(tones2, ['tone_2000'] * 5)
-    tones1 = np.append(tones1, ['tone_2000'] * 5)
-    tones2 = np.append(tones2, ['tone_500'] * 5)
+    tones1 = np.append(tones1, ['tone_500.wav'] * 5)
+    tones2 = np.append(tones2, ['tone_2000.wav'] * 5)
+    tones1 = np.append(tones1, ['tone_2000.wav'] * 5)
+    tones2 = np.append(tones2, ['tone_500.wav'] * 5)
     df = pd.DataFrame()
     df['type'] = types
     df['tone1'] = tones1
     df['gap'] = npr.randint(2000, 3000, size=len(df))
     df['tone2'] = tones2
+    df['after'] = 5000
     return df.sample(len(df))
 
 
