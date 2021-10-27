8cae8e6ddd1d200e6e8ec903eda0b3eac6cb528f
Matthew Brett <matthew.brett@gmail.com>
2021-10-27 10:45:36 +0100

    Set amplitude of tones.

--- make_tones.py
+++ make_tones.py
@@ -13,14 +13,14 @@ clips = {'tone_500': {'freq': 500,
 
 def make_tone(freq, duration, rate=44100):
     secs = duration / 1000
-    print(freq)
     n_samps = int(rate * secs)
     t = np.linspace(0., secs, n_samps)
     return np.sin(2. * np.pi * freq * t)
 
 
-def write_tone(fname, cdict, rate=44100):
+def write_tone(fname, cdict, rate=44100, amplitude=1):
     tone = make_tone(rate=rate, **cdict).astype(np.float32)
+    tone = tone / np.max(np.abs(tone)) * amplitude
     siow.write(fname, rate, tone)
 
 
